4f49ff01671fb12d14998fb5017af1c5def496a4
Sergey Pavlov <s.a.pavlov@mail.ru>
2022-12-08 23:36:25 +0300

    add SPQR

--- src/main/java/com/example/familygraphql/controller/FamilyController.java
+++ src/main/java/com/example/familygraphql/controller/FamilyController.java
@@ -2,30 +2,33 @@ package com.example.familygraphql.controller;
 
 import java.util.List;
 
-import org.springframework.graphql.data.method.annotation.Argument;
-import org.springframework.graphql.data.method.annotation.MutationMapping;
-import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 import com.example.familygraphql.exeption.FamilyNotFoundExeption;
 import com.example.familygraphql.model.FamilyEntity;
 import com.example.familygraphql.repository.FamilyRepository;
 
+import io.leangen.graphql.annotations.GraphQLArgument;
+import io.leangen.graphql.annotations.GraphQLMutation;
+import io.leangen.graphql.annotations.GraphQLQuery;
+import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
+@GraphQLApi
 public class FamilyController {
 
 	private final FamilyRepository familyRepository;
 	
-	@QueryMapping
+	@GraphQLQuery(name="findAllFamilies")
 	public List<FamilyEntity> findAllFamilies(){
 		return familyRepository.findAll();
 	}
 	
-	@MutationMapping
-	public FamilyEntity createFamily(@Argument String surname,@Argument String name,@Argument String fathername) {
+	@GraphQLMutation
+	public FamilyEntity createFamily(@GraphQLArgument(name="surname") String surname,@GraphQLArgument(name="name") String name,
+			@GraphQLArgument(name="fathername") String fathername) {
 		FamilyEntity family=new FamilyEntity();
 		family.setSurname(surname);
 		family.setName(name);
@@ -36,8 +39,10 @@ public class FamilyController {
 		return family;
 	}
 	
-	@MutationMapping
-	public FamilyEntity updateFamily(@Argument Long id,@Argument String surname,@Argument String name,@Argument String fathername){
+	@GraphQLMutation
+	public FamilyEntity updateFamily(@GraphQLArgument(name="id") Long id,@GraphQLArgument(name="surname") String surname,
+			@GraphQLArgument(name="name") String name,
+			@GraphQLArgument(name="fathername") String fathername){
 		
 		FamilyEntity family=familyRepository.findById(id).orElseThrow(()->new FamilyNotFoundExeption("Not found FamilyEntity to update!"));
 		
@@ -50,8 +55,8 @@ public class FamilyController {
 		return family;
 	}
 	
-	@MutationMapping
-	public boolean deleteFamily(@Argument Long id) {
+	@GraphQLMutation
+	public boolean deleteFamily(@GraphQLArgument(name="id") Long id) {
 		familyRepository.deleteById(id);
 		
 		return true;
--- src/main/java/com/example/familygraphql/model/FamilyEntity.java
+++ src/main/java/com/example/familygraphql/model/FamilyEntity.java
@@ -8,6 +8,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import io.leangen.graphql.annotations.GraphQLId;
+import io.leangen.graphql.annotations.GraphQLInputField;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,16 +23,21 @@ public class FamilyEntity {
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GraphQLId
 	private Long id;
 	
 	@Column(name="surname")
+	@GraphQLInputField
 	private String surname;
 
 	@Column
+	@GraphQLInputField
 	private String name; //(name="name") можно не указывать, если название совпадает с названием поля
 
+	@GraphQLInputField
 	private String fathername; //Column и (name="fathername") можно не указывать, если название совпадает с названием поля
 
 	@Column
+	@GraphQLInputField
 	private LocalDate birthday;
 }
